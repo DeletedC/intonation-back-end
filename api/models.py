205b93748c1be6d0b415bb6e969c41fe4ff9a7d3
Curtis <CurtisWoodsArt@gmail.com>
2020-06-30 23:49:07 -0400

    Started Student and Note models

--- api/models.py
+++ api/models.py
@@ -1,4 +1,28 @@
 from django.db import models
-from django.utils.text import slugify
+#from django.utils.text import slugify
+
 
 # Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    #age
+    #book
+    #level
+
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
+    def save(self, *args, **kwargs):
+        return super(Student, self).save(*args, **kwargs)
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '%s %s' % (self.title, self.body)
